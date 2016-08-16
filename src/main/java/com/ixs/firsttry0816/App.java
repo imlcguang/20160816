fb77a5b042b03bc581d2c94d121ff5221899b642
imlcguang <490065341@qq.com>
2016-08-16 17:12:20 +0800

    spring

---
+++ src/main/java/com/ixs/firsttry0816/App.java
@@ -0,0 +1,13 @@
+package com.ixs.firsttry0816;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
